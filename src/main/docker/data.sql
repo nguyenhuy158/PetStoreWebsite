13d51f13461718f4e989d3f292552e0e7ddf3644
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-03-28 19:18:47 +0700

    edit data.sql

---
+++ src/main/docker/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PetStore.`user` (address,email,gender,name,password,phone,username) VALUES
+	 (NULL,NULL,NULL,NULL,'$2a$10$hvc.LItyVcFxPELQdBtneudTx2JcIIgBFhWFPVjSC37cBT9pdPGUO',NULL,'M60-099022@internet'),
+	 (NULL,NULL,NULL,NULL,'$2a$10$7c8m6d0Wtst5omV4zx.hjOjxLSKj7CNYfDnablIXjt24xxXjdTF5a',NULL,'123123');
