86d21b2149c30fbd5bf03c1306992f0d2f495b35
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-03-28 07:48:10 +0700

    native sql

--- src/main/java/vn/petstore/website/repository/PetRepository.java
+++ src/main/java/vn/petstore/website/repository/PetRepository.java
@@ -1,6 +1,8 @@
 package vn.petstore.website.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import vn.petstore.website.model.Pet;
@@ -8,4 +10,6 @@ import vn.petstore.website.model.Pet;
 @Component
 @Repository
 public interface PetRepository extends JpaRepository<Pet, Long> {
+    @Query(value = "SELECT * FROM Pet p WHERE p.name = :name and p.age = :age", nativeQuery = true)
+    Pet findPetByNamedAndAgeNative(@Param("name") Integer name, @Param("age") String age);
 }
