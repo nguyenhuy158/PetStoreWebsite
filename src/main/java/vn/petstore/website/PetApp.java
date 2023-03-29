9f06a14dfc71c82844aa67d6b8bc967237c883f0
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-03-29 17:24:42 +0700

    feature: login

--- src/main/java/vn/petstore/website/PetApp.java
+++ src/main/java/vn/petstore/website/PetApp.java
@@ -1,16 +1,33 @@
 package vn.petstore.website;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import vn.petstore.website.model.User;
+import vn.petstore.website.repository.UserRepository;
 
 @SpringBootApplication
 @RequiredArgsConstructor
-public class PetApp {
+public class PetApp implements CommandLineRunner {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(PetApp.class, args);
-	}
+        SpringApplication.run(PetApp.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setUsername("ntqhuy2k2");
+        user.setPassword(passwordEncoder.encode(" "));
+        userRepository.save(user);
+        System.out.println(user);
+    }
 }
---
+++ src/main/java/vn/petstore/website/config/SecurityConfiguration.java
@@ -0,0 +1,60 @@
+package vn.petstore.website.config;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import vn.petstore.website.services.CustomUserDetailService;
+
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfiguration {
+
+    @Autowired
+    CustomUserDetailService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers("/about").permitAll() // Cho phép tất cả mọi người truy cập vào 2 địa chỉ này
+                .anyRequest().authenticated() // Tất cả các request khác đều cần phải xác thực mới được truy cập
+                .and()
+                .formLogin() // Cho phép người dùng xác thực bằng form login
+                .defaultSuccessUrl("/home")
+                .permitAll() // Tất cả đều được truy cập vào địa chỉ này
+                .and()
+                .logout() // Cho phép logout
+                .permitAll();
+
+        return http.build();
+    }
+
+//    @Bean
+//    public WebSecurityCustomizer webSecurityCustomizer() {
+////        return (web) -> web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
+//    }
+
+
+}
