72e40e4d82cb3566c917005ad54d3bac05c3a4ad
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-03-29 09:40:12 +0700

    add script script-restore-db.sh

--- src/main/docker/script-backup-db.sh
+++ src/main/docker/script-backup-db.sh
@@ -1,2 +1,2 @@
 rm data.sql
-sudo docker exec d933dc7030cf /usr/bin/mysqldump -u root --password=petstoremaidinh PetStore > data.sql
+sudo docker exec c219c72151f4 /usr/bin/mysqldump -u root --password=petstoremaidinh PetStore > data.sql
