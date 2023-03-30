5ef1caa1f10cfd172a075f034218658cc9e2a0f6
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-03-31 02:16:43 +0700

    update pet

--- src/main/java/vn/petstore/website/PetApp.java
+++ src/main/java/vn/petstore/website/PetApp.java
@@ -29,6 +29,10 @@ public class PetApp implements CommandLineRunner {
             User user = new User();
             user.setUsername("1");
             user.setPassword(passwordEncoder.encode("1"));
+            user.setName("Nguyen Huy");
+            user.setPhone("0837377855");
+            user.setAddress("Quan 7, HCM");
+            user.setEmail("ntqhuy2k2@gmail.com");
             userRepository.save(user);
             System.out.println(user);
         }
