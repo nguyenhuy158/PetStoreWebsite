f4e783f03c3c2954b127020243d02dfdd255b8d5
ntqhuy2k2 <ntqhuy2k2@gmail.com>
2023-04-24 08:17:36 +0700

    update jwtfilter

---
+++ src/main/java/vn/petstore/website/jwtutils/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package vn.petstore.website.jwtutils;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException)
+            throws IOException,
+            ServletException {
+        response.sendError(
+                HttpServletResponse.SC_UNAUTHORIZED,
+                "Unauthorized");
+    }
+}
